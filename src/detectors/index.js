4e81f87543664cf68a86fb2a8acbd08f4daa31c3
andiselimi <andiselimi24@gmail.com>
2026-02-20 19:04:11 +0100

    baseline for random engagement detector, and the recurring events detector

---
+++ src/detectors/index.js
@@ -0,0 +1,7 @@
+const RandomEngagementDetector = require('./RandomEngagementDetector')
+const RecurringEventDetector = require('./RecurringEventDetector')
+
+module.exports = [
+  RecurringEventDetector,
+  RandomEngagementDetector
+]
